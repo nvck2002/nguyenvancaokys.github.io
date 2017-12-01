987ceb0da55751153fda549eb6a2924ee9b17363
nguyenvancaoky <nguyenvancaoky@gmail.com>
2017-12-01 13:33:43 +0700

    Upload Project

---
+++ system.js
@@ -0,0 +1,2 @@
+document.getElementsByTagName("H1")[0].setAttribute("class", "w3-panel w3-leftbar w3-border-teal w3-text-teal w3-container w3-animate-top");
+document.getElementsByTagName("BUTTON")[0].setAttribute("class", "w3-button w3-teal"); 
